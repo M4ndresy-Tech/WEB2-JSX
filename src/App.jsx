ec81724789a6e93aadc3d19a3911d54c59ae82eb
Mandresy <hei.mandresy.3@gmail.com>
2025-07-02 14:10:45 +0200

    First commit - setup React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+    <p className='text-2xl text-blue-700'>Hello world</p>
+
+    </div>
+
+    </> 
+  )
+}
+
+export default App
