3bd8d6e2932ab6abf6ff9aad0324a5a5c628d378
Mandresy <hei.mandresy.3@gmail.com>
2025-07-02 17:49:59 +0200

    Built the header with a responsive navabar

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
-    <p className='text-2xl text-blue-700'>Hello world</p>
-
-    </div>
-
+   <Header/>
+    
     </> 
-  )
+  );
 }
 
 export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "../assets/cropped-Logo-e1662790239183.png";
+
+const Header = () => {
+  return (
+    <header className="w-full bg-blue-950 py-4">
+      <nav className="max-w-screen-xl mx-auto px-4 flex flex-col items-center md:flex-row md:justify-between">
+        
+        {/* Logo centré sur mobile, à gauche sur desktop */}
+        <div className="mb-4 md:mb-0">
+          <img src={logo} alt="Logo HEI" className="w-24 h-auto mx-auto md:mx-0" />
+        </div>
+
+        
+        <div className="flex flex-col items-center gap-4 md:flex-row  md:space-x-10 text-amber-50 font-bold text-lg">
+          <div className="cursor-pointer text-amber-500 hover:text-amber-400">ACCUEIL</div>
+          <div className="cursor-pointer hover:text-amber-400">ACTUALITÉS</div>
+          <div className="cursor-pointer hover:text-amber-400">BOURSE D'ÉTUDES</div>
+          <div className="cursor-pointer hover:text-amber-400">INSCRIPTION</div>
+          <button className="bg-amber-400 text-blue-950 rounded px-4 py-2 hover:text-blue-950">
+            INTRANET
+          </button>
+        </div>
+
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
