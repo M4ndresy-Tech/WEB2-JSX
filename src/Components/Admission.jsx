1749efb18f78b0fe4c00741fababfd4c7ec76b84
Mandresy <hei.mandresy.3@gmail.com>
2025-07-03 00:21:34 +0200

    Edit for the Admition section

--- src/Components/Admission.jsx
+++ src/Components/Admission.jsx
@@ -9,21 +9,21 @@ const Admission=()=>{
             </div>
 
             <div className="flex flex-col md:flex-row md:justify-center p-20">
-                <div className="flex flex-col space-y-4 w-1/3 bg-orange-200 p-12">
+                <div className="flex flex-col space-y-4 md:w-1/3 w-2/3 bg-orange-200 p-12">
                     <div className=" text-5xl text-white font-bold"><h1>01</h1></div>
                     <div className="flex flex-row justify-around">
                         <div className="text-blue-950 text-lg "><p><span className="font-bold">Dépôt de dossiers :</span> pour les bacheliers de toutes séries sans limite d’âge</p></div>
                         <div className="text-5xl text-white my-2"><i class="fa-solid fa-arrow-right"></i></div>
                     </div>
                 </div>
-                <div className="flex flex-col space-y-4 w-1/3 bg-orange-300 p-10">
+                <div className="flex flex-col space-y-4 md:w-1/3 w-2/3 bg-orange-300 p-10">
                     <div className="text-5xl text-white font-bold"><h1>02</h1></div>
                     <div className="flex flex-row justify-around">
                     <div className="text-blue-950 text-lg "><p><span className="font-bold">Test de niveau :</span> composé d’une épreuve de français niveau B2 et d’une épreuve de mathématiques niveau Terminale D</p></div>
                     <div className="text-5xl text-white my-3"><i class="fa-solid fa-arrow-right"></i></div>
                     </div>
                 </div>
-                <div className="flex flex-col space-y-4 w-1/3 bg-amber-500 p-12">
+                <div className="flex flex-col space-y-4 md:w-1/3 w-2/3 bg-amber-500 p-12">
                     <div className="text-5xl text-white font-bold"><h1>03</h1></div>
                     <div className="text-blue-950 text-lg "><span className="font-bold">Inscription définitive </span>(si test réussi)</div>
                 </div>
